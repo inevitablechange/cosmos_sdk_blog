bdf5189126b50afb53a508f1962906b078cdb42a
inevitablechange <yongho7783@gmail.com>
2025-12-07 12:33:43 +0900

    feat(blog): add BlogPostsKey and MaxIDLength constants for blog post storage

--- chain-minimal/x/blog/keys.go
+++ chain-minimal/x/blog/keys.go
@@ -5,5 +5,8 @@ import "cosmossdk.io/collections"
 const ModuleName = "blog"
 
 var (
-    ParamsKey  = collections.NewPrefix("Params")
+	ParamsKey      = collections.NewPrefix("Params")           // 나중에 쓸 수도 있음
+	BlogPostsKey   = collections.NewPrefix("BlogPosts/value/") // 모듈에서 글들을 저장할 때 'BlogPosts/value/{id}' 이런 식의 key로 저장되게끔 해줌
+	MaxIDLength    = 256
 )
+
