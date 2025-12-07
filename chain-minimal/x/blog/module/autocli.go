4bf539dc5bd67f99debacdc6469f1500447954ad
inevitablechange <yongho7783@gmail.com>
2025-12-07 12:33:49 +0900

    feat(blog): implement AutoCLIOptions for blog post queries and creation

--- chain-minimal/x/blog/module/autocli.go
+++ chain-minimal/x/blog/module/autocli.go
@@ -2,12 +2,43 @@ package module
 
 import (
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+
+	blogv1 "blockchainvalley.io/bvchain/x/blog/api/v1"
 )
 
 // AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
 func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
-    return &autocliv1.ModuleOptions{
-        Query: nil,
-        Tx:    nil,
-    }
+	return &autocliv1.ModuleOptions{
+		// Query 쪽 CLI
+		Query: &autocliv1.ServiceCommandDescriptor{
+			Service: blogv1.Query_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "GetPost",
+					Use:       "get-post [id]",
+					Short:     "Get a blog post by id",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "id"},
+					},
+				},
+			},
+		},
+
+		// Tx 쪽 CLI
+		Tx: &autocliv1.ServiceCommandDescriptor{
+			Service: blogv1.Msg_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "CreatePost",
+					Use:       "create-post [id] [title] [contents]",
+					Short:     "Create a new blog post with given id, title, and contents",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "id"},
+						{ProtoField: "title"},
+						{ProtoField: "contents"},
+					},
+				},
+			},
+		},
+	}
 }
